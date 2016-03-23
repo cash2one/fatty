50f5c4fa1d57edc22561c7380433090e711f3bd2
Runan <527048226@qq.com>
2016-03-24 00:15:25 +0800

    overall

--- one.py
+++ one.py
@@ -1,13 +1,18 @@
 import os
-def traversal(A_path,b_path):
-    dir=os.listdir(A_path)
+def traversal(path):
+    dir=os.listdir(path)
     print(dir)
     for each in dir:
-        if each == "dog.txt":
-            print("dog.txt"+"at"+"A_path" + "b_patch")
-if __name__ == '__main__':
-	A_path = "D:\\"
-	b_path = "C"
-	traversal(A_path,b_path)
+      if each is None:
+        print("Have nothing!")
+      elif each == "dog.txt":
+          file_path = path + os.path.sep + each + os.path.sep + "dog.txt"
+          print("dog.txt"+"at"+ file_path)
+      elif each is None:
+        break
+      else:
+        traversal(each)
+A_path="c:"
+traversal(A_path)
     
 
